f19d282a17e7669c4bc6af9caf702ea97e4f2235
gilhun <qkrrlfgns123@gmail.com>
2023-12-06 10:38:37 +0900

    로그인 만드는 중

---
+++ src/page/Login.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { auth } from "./firebase";
+
+const login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      console.log("로그인 성공");
+    } catch (error) {
+      console.error("로그인 실패", error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <input
+        type="email"
+        placeholder="이메일"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="비밀번호"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>로그인</button>
+    </div>
+  );
+};
+
+export default login;
