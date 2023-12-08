dc38543cc33fc027308a0b56e05ee057252e7c4c
최문길 <cmk0905@naver.com>
2023-12-08 09:08:09 +0900

    원장님께 여쭤볼것

--- src/page/Sample/SampleKakao.jsx
+++ src/page/Sample/SampleKakao.jsx
@@ -1,14 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react'
+import styled from 'styled-components'
+
 function SampleKakao() {
   const kakaoMapRef = useRef()
   const [map, setMap] = useState(null)
+  const { kakao } = window
   const [coords, setCoords] = useState({
     lat: 37.566498652285,
     lng: 126.99209745028,
   })
 
-  // 사용자 위치정보 허용했을 경우 해당 위도/경도 설정 - Geolocation은 navigator.geolocation을 통해 접근하고, 사용자가 허가할 경우 현재 장치에서 GPS, Wifi 등 사용 가능한 최선의 방법으로 위치를 알아낸다.
-  // position은 navigator.geolocation.getCurrentPosition을 실행 시킬때 숨겨져있는 argu
+  // input창에 주소 keyword 등을 검색하면, 뾰로롱 하고 해당 지도의 위치가 바뀌고, 마커등록하기
+  const inputRef = useRef()
+  const [keyWord, setKeyWord] = useState('')
+  const [markers, setMarkers] = useState()
+  const [search, setSearch] = useState(false)
+  const onClickFindGeoLocation = () => {
+    console.log(window.kakao.maps)
+
+    // console.log(geocoder)
+    // geocoder.addressSearch(inputRef.current.value, (result, status) => {
+    //   console.log(status)
+    //   if (status === kakao.maps.services.Status.OK) {
+    //     setCoords(new kakao.maps.LatLng(result[0].y, result[0].x))
+    //   }
+    // })
+  }
+
   const getUserCurrentLocation = (pos) =>
     setCoords((prev) => ({
       ...prev,
@@ -20,7 +38,6 @@ function SampleKakao() {
   const failedUserCurrentLocation = () => console.log('안타깝군요')
   // 자동 함수 실행시켜주자 useEffect안에서 - 그런데 머부터 실행이 될까?>
   useEffect(() => {
-    console.log(1)
     navigator.geolocation.getCurrentPosition(
       getUserCurrentLocation,
       failedUserCurrentLocation
@@ -29,10 +46,8 @@ function SampleKakao() {
 
   // 처음 kakaoMAp 생성되는 useEffect입니다.
   useEffect(() => {
-    if (map) return
     const kakaoMapScript = document.createElement('script')
     kakaoMapScript.async = true
-
     kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_JAVASCRIPT_KEY}&autoload=false`
 
     document.head.appendChild(kakaoMapScript)
@@ -42,14 +57,14 @@ function SampleKakao() {
           center: new window.kakao.maps.LatLng(coords.lat, coords.lng),
           level: 3,
         }
+        const geocoder = new window.kakao.maps.services.Geocoder()
         const map = new window.kakao.maps.Map(kakaoMapRef.current, options)
         const marker = new window.kakao.maps.Marker({
           position: new window.kakao.maps.LatLng(coords.lat, coords.lng),
-          // map은 45번라인 생성해준 map이고, 만들었으면 어디다가 보여줄건디;; 만들어준 map에다 보여줄거잖아 맞아?
           map: map,
         })
         const infowindow = new window.kakao.maps.InfoWindow({
-          content: '처음입니다.',
+          content: '혹시 지금 위치입니까?.',
           position: new window.kakao.maps.LatLng(coords.lat, coords.lng),
         })
 
@@ -57,10 +72,33 @@ function SampleKakao() {
         setMap(map)
       })
     }
-  }, [map])
+  }, [coords])
+  useEffect(() => {
+    if (!search) return
+  }, [search])
   return (
-    <div ref={kakaoMapRef} style={{ width: '400px', height: '300px' }}></div>
+    <Div>
+      <form action="" onClick={(e) => e.preventDefault()}>
+        <input type="text" ref={inputRef} />
+        <button onClick={onClickFindGeoLocation}>검색하기</button>
+      </form>
+      <div ref={kakaoMapRef} style={{ width: '400px', height: '300px' }}></div>
+      {markers?.map()}
+    </Div>
   )
 }
-
+// if (!keyword.replace(/^\s+|\s+$/g, '')) {
+//   alert('키워드를 입력해주세요!');
+//   return false;
+// }
 export default SampleKakao
+
+const Div = styled.div`
+  display: grid;
+  place-content: center center;
+  height: 100vh;
+  gap: 20px;
+  input {
+    padding: 10px;
+  }
+`
