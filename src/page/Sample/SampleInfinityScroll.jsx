3d6d7f860f541289792e7c479324c2a6e6b3f6c9
최문길 <cmk0905@naver.com>
2023-12-11 08:43:19 +0900

    smaple

--- src/page/Sample/SampleInfinityScroll.jsx
+++ src/page/Sample/SampleInfinityScroll.jsx
@@ -10,23 +10,25 @@ function SampleInfinityScroll() {
   const [noneTitle, setNoneTitle] = useState(false)
   let cancel
 
-  // 태그안에 ref를 함수도 넣을 수 있더라.... 그러면 node가 바로 ref인 태그 요소임! 그리고 함수가 ref로서 current를 가지게 됨
-  const observeRef = (node) => {
-    //   // 맨 아래 요소를 새롭게 계속해서 갱신하는 거임- 1. loading이면 종료 2. 맨 처음 마지막 참조였던것은 observer를 끊어주고 다시 새롭게 참조를 만들어 주기 위함 3. 새로운 마지막 요소에 관찰자를 넣음
-    if (observeRef.current) observeRef.current.disconnect()
-    if (node) {
-      if (isLoading) return
-      // 참조하고 있는 observer 먼저 끊어 주기, 근데 어떻게 먼저 끊어주냐 current에 먼가 참조해주는 것보다 먼저??-> if문 있잖냐...
-      if (observeRef.current) observeRef.current.disconnect()
-      observeRef.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) {
-          setPageNumber((prev) => prev + 1)
-        }
-      })
-      if (node) observeRef.current.observe(node)
-    }
-  }
+  // // 태그안에 ref를 함수도 넣을 수 있더라.... 그러면 node가 바로 ref인 태그 요소임! 그리고 함수가 ref로서 current를 가지게 됨
+  // const observeRef = (node) => {
+  //   //   // 맨 아래 요소를 새롭게 계속해서 갱신하는 거임- 1. loading이면 종료 2. 맨 처음 마지막 참조였던것은 observer를 끊어주고 다시 새롭게 참조를 만들어 주기 위함 3. 새로운 마지막 요소에 관찰자를 넣음
 
+  //   if (observeRef.current) observeRef.current.disconnect()
+  //   if (node) {
+  //     if (isLoading) return
+  //     // 참조하고 있는 observer 먼저 끊어 주기, 근데 어떻게 먼저 끊어주냐 current에 먼가 참조해주는 것보다 먼저??-> if문 있잖냐...
+  //     if (observeRef.current) observeRef.current.disconnect()
+
+  //     observeRef.current = new IntersectionObserver((entries) => {
+  //       if (entries[0].isIntersecting) {
+  //         setPageNumber((prev) => prev + 1)
+  //       }
+  //     })
+  //     if (node) observeRef.current.observe(node)
+  //   }
+  // }
+  console.log(isLoading)
   // useCallback써준 이유는 useCallback 풀고 해봐라 무한반복임
   const queryData = useCallback(
     async (q, number) => {
@@ -45,7 +47,6 @@ function SampleInfinityScroll() {
             cancelToken: new axios.CancelToken((c) => (cancel = c)),
           })
           .then((res) => {
-            console.log(res.data.docs)
             setIsLoading(false)
             // 없으면 없다고 친절하게 알려줘야 하니까
             if (res.data.docs.length === 0) return setNoneTitle(true)
@@ -70,14 +71,16 @@ function SampleInfinityScroll() {
 
   // 검색하고 검색또하면 내용들 쭈르륵임그러기에  최신화 해주자
   useEffect(() => {
+    console.log('1')
     setBooks([])
   }, [query])
 
   useEffect(() => {
+    console.log('2')
     queryData(query, pageNumber)
     return () => cancel()
   }, [query, pageNumber, cancel, queryData])
-  console.log('return안에 ', isLoading)
+
   return (
     <>
       <input type="text" value={query} onChange={handleSearch} />
